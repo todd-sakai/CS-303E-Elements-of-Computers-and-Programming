94e590b5e1a2f44e0c267d89c6274173780f7de2
Todd Sakai <todd.sakai@outlook.com>
2026-06-29 23:00:15 -1000

    Add files via upload

---
+++ Homework 1 Initials/Initials.py
@@ -0,0 +1,37 @@
+# Assignment: HW1
+# File: Initials.py
+# Student: Todd Sakai 
+# UT EID: tfs523
+# Course: C S 303E
+# 
+# Date: 9.3.25
+# Description of Program: Outputs my initials "T.F.S" in large letters with a width of 12 letters and a height of 10 letters
+
+def initials():
+    #Assign variables to lines of initials
+        line0="                                                    "
+        line1="  TTTTTTTTTTTT      FFFFFFFFFFFF         SSSSSS     "
+        line2="  TTTTTTTTTTTT      FFFFFFFFFFFF       SSSS  SSSS   "
+        line3="       TT           FF                SSS      SSS  "
+        line4="       TT           FF                SSS       SS  "
+        line5="       TT           FFFFFFFFFFFF       SSSSSSS      "
+        line6="       TT           FFFFFFFFFFFF          SSSSSSS   "
+        line7="       TT           FF                SS       SSS  "
+        line8="       TT           FF                SSS      SSS  "
+        line9="       TT       ..  FF            ..   SSSS  SSSS   "
+        line10="       TT       ..  FF            ..     SSSSSS     "
+        line11="                                                    "
+
+    #Print lines one-by-one so they line up properly
+        print(line0)
+        print(line1)
+        print(line2)
+        print(line3)
+        print(line4)
+        print(line5)
+        print(line6)
+        print(line7)
+        print(line8)
+        print(line9)
+        print(line10)
+        print(line11)
